9067af4be2e8fbd52731a242bbdf0a97262add56
Michael Voorhees <michaelv@unity3d.com>
2019-02-08 12:41:58 -0500

    Formatting fix to UnityOptions

--- Unity.Options.Tests/ResponseFileTests.cs
+++ Unity.Options.Tests/ResponseFileTests.cs
@@ -11,41 +11,41 @@ namespace Unity.Options.Tests
         {
             using (var tempFile = TempFile.CreateRandom())
             {
-                File.WriteAllLines(tempFile.Path, new []
+                File.WriteAllLines(tempFile.Path, new[]
                 {
                     "--value=10"
                 });
-                
+
                 var commandLine = new[]
                 {
                     $"@{tempFile.Path}"
                 };
-                
+
                 var types = new[] { typeof(SimpleOptions) };
-                
+
                 OptionsParser.Prepare(commandLine, types);
 
                 Assert.AreEqual(10, SimpleOptions.Value);
             }
         }
-        
+
         [Test]
         public void SimpleRelativePathResponseFileWorks()
         {
             using (var tempFile = TempFile.CreateRandom())
             {
-                File.WriteAllLines(tempFile.Path, new []
+                File.WriteAllLines(tempFile.Path, new[]
                 {
                     "--value=10"
                 });
-                
+
                 var commandLine = new[]
                 {
                     $"@{Path.GetFileName(tempFile.Path)}"
                 };
-                
+
                 var types = new[] { typeof(SimpleOptions) };
-                
+
                 OptionsParser.Prepare(commandLine, types, currentDirectory: Path.GetDirectoryName(tempFile.Path));
 
                 Assert.AreEqual(10, SimpleOptions.Value);
@@ -61,9 +61,9 @@ namespace Unity.Options.Tests
                 {
                     $"@{tempFile.Path}"
                 };
-                
+
                 var types = new[] { typeof(SimpleOptions) };
-                
+
                 Assert.Throws<FileNotFoundException>(() => OptionsParser.Prepare(commandLine, types));
             }
         }
